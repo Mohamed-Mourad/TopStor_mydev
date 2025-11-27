7576d4b23b3973f6a29aed1918d67bf6a3f553b1
sdlfkj <dkdjkl@dljkf.com>
2025-11-27 18:23:38 +0200

    fixing

--- allphysicalinfo.py
+++ allphysicalinfo.py
@@ -135,7 +135,8 @@ def getall(leadip, alldsks='0'):
     poolsdict[pool['name']]['Availability'] = 'None'
    for raid in pool['raidlist']:
     if 'free' not in raid['name']:
-     raidname = raid['name']+'_'+pool['name']
+     if disk['name'] in raidsdict['free']['disks']:
+      raidname = raid['name']+'_'+pool['name']
     else:
       raidname = raid['name']
     poolraids.append(raidname)
